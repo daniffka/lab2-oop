bbc9b85dc3ff0c659d430efa17101782dfaaf425
Berbentsev <denberben@gmail.com>
2024-10-10 01:40:41 +0400

    y part

--- src/main/java/ru/ssau/tk/BerbentsevBalabashin/functions/MathFunction.java
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/functions/MathFunction.java
@@ -1,7 +1,5 @@
 package ru.ssau.tk.BerbentsevBalabashin.functions;
 
-import ru.ssau.tk.BerbentsevBalabashin.CompositeFunction;
-
 public interface MathFunction {
     double apply(double x);
     default CompositeFunction andThen(MathFunction afterFunction){
